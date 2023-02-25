5ed1ee482ea3fe0813f889a415e8a1b60bcc5626
onkii-kalshetti <125990841+onkii-kalshetti@users.noreply.github.com>
2023-02-25 11:35:00 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -13,7 +13,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple'])
 streamlit.dataframe(my_fruit_list)
+streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response())
