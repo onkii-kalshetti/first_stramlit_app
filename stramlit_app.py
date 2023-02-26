77204f136b9981bda098e56ebbdf08c8bf4fbb53
onkii-kalshetti <125990841+onkii-kalshetti@users.noreply.github.com>
2023-02-26 13:02:35 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -18,15 +18,23 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Strawberries'])
 streamlit.dataframe(my_fruit_list)
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   if not fruit_choice:
+      streamlit.error("Please select a fruit to get information")
+   else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+except URLERROR as e:
+  streamlit.error()
+    
 streamlit.write('The user entered ', fruit_choice)
 add_my_fruit=streamlit.text_input('What fruit would you like information about?','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+
 #streamlit.text(fruityvice_response.json())
 streamlit.stop()
 
