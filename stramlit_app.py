204dc86687914415054d091d317a31c56e195e07
onkii-kalshetti <125990841+onkii-kalshetti@users.noreply.github.com>
2023-02-26 13:19:30 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -33,8 +33,8 @@ try:
       #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       #streamlit.dataframe(fruityvice_normalized)
-except URLERROR as e:
-  streamlit.error()
+#except URLERROR as e:
+  #streamlit.error()
     
 streamlit.write('The user entered ', fruit_choice)
 add_my_fruit=streamlit.text_input('What fruit would you like information about?','Jackfruit')
