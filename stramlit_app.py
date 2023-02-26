7df9f2ceed8811293ee74730fbd9ac95a034e9ee
onkii-kalshetti <125990841+onkii-kalshetti@users.noreply.github.com>
2023-02-26 15:00:34 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -41,12 +41,13 @@ add_my_fruit=streamlit.text_input('What fruit would you like information about?'
 streamlit.write('Thanks for adding ', add_my_fruit)
 #streamlit.text(fruityvice_response.json())
 #streamlit.stop()
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List -Add Our Favourites!")
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from fruit_load_list")
          return my_cur.fetchall()
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit  List'):
+   
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows=get_fruit_load_list()
    my_cnx.close()
