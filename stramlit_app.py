ae11a481ca137be20446385de4616f1d0721cb90
onkii-kalshetti <125990841+onkii-kalshetti@users.noreply.github.com>
2023-02-26 13:34:23 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -45,10 +45,17 @@ streamlit.write('Thanks for adding ', fruit_choice)
 #streamlit.text(fruityvice_response.json())
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchall()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+
+#my_data_row = 
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows=get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
